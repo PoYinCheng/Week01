a7030b906f9d14be4b5d9f1d680e2dcd9dbf2ba9
PoYinCheng <mit1994723@gmail.com>
2015-03-01 19:42:10 +0800

    七言絕句
    七言絕句

---
+++ seven.java
@@ -0,0 +1,9 @@
+class Hello {
+	public static void main(String[] args) {
+		System.out.println("　　絕句　杜甫");
+		System.out.println("兩個黃鸝鳴翠柳，");
+		System.out.println("一行白鷺上青天。");
+		System.out.println("窗含西嶺千秋雪，");
+		System.out.println("門泊東吳萬里船。");
+	}
+}
